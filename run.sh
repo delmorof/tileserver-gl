d8b427e9780ed248d2be0a8cb3fe79f6fc6de177
Filippo Del Moro <delmorof@gmail.com>
2016-09-06 10:40:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /data
-xvfb-run -a -e /dev/stdout --server-args="-screen 0 1024x768x24" node /usr/src/app/ -p 80 -c config.json ${INPUT}
+xvfb-run -a -e /dev/stdout --server-args="-screen 0 1024x768x24" node /usr/src/app/ -p 80 -c /usr/src/app/config.json -V ${INPUT}
